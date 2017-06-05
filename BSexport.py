84857589a135ebacdab48ce026e7adb9142b8b9c
Davinci342 <N00933776@ospreys.unf.edu>
2017-06-05 16:40:04 -0400

    Update BSexport.py

--- BSexport.py
+++ BSexport.py
@@ -13,15 +13,17 @@ ArrayUp = np.spinup[BandNumber].reshape[BandNumber,2]
 ArrayDown = np.spindown[BandNumber].reshape[BandNumber,2]
 
 for BandIteration in range(0,BandNumber):
-    fh.readoline()
+    fh.read(line)
     def chop_comment(line):#need to look over
         in_quote = False
         backslash_escape = False
         for i, ch in enumerate(line):
             if not in_quote and ch == c_comment:
                 return line[:i]
-    return line    
-while not '\n\n':
+    return line
+if True:
+       fh.readline(c_comment)
+        while not '\n\n':
                 if True:#trying to get program to recognize spin up and spin down lines
                     fh.readline('Spin.Up')
                     store in ArrayUp  #this might only store the lines that define the bands, not the actual lines that contain data
