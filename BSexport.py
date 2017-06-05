2fd4f07f687aab69599e6ecfb8474807251456ba
Davinci342 <N00933776@ospreys.unf.edu>
2017-06-05 15:40:27 -0400

    Add files via upload

---
+++ BSexport.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 12:26:42 2017
+
+@author: MaterialsTheory
+"""
+
+fh = open("test.txt","r") 
+import numpy as np
+BandNumber= 1641
+c_comment = '#'
+char == Spin.Up
+ArrayUp = np.spinup[BandNumber].reshape[BandNumber,2]
+ArrayDown = np.spindown[BandNumber].reshape[2,BandNumber]
+
+for BandIteration in range(0,BandNumber):
+
+    fh.read(line)
+    
+while not '\n\n':
+            if char in 
+                if
+                    import in ArrayUp
+                else:
+                    import in ArrayDown
+return ArrayUp
+return ArrayDown
+print ArrayUp
+print ArrayDown
+fh.close()
