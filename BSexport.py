4f1b65dea925e4f7a7c80f187dbf67c8580b80a6
Davinci342 <N00933776@ospreys.unf.edu>
2017-06-05 16:25:13 -0400

    Update BSexport.py

--- BSexport.py
+++ BSexport.py
@@ -9,20 +9,26 @@ fh = open("test.txt","r")
 import numpy as np
 BandNumber= 1641
 c_comment = '#'
-char == Spin.Up
 ArrayUp = np.spinup[BandNumber].reshape[BandNumber,2]
 ArrayDown = np.spindown[BandNumber].reshape[BandNumber,2]
 
 for BandIteration in range(0,BandNumber):
-    
     fh.read(line)
-    
+    def chop_comment(line):#need to look over
+        in_quote = False
+        backslash_escape = False
+        for i, ch in enumerate(line):
+            if not in_quote and ch == c_comment:
+                return line[:i]
+    return line    
 while not '\n\n':
-            if char in 
-                if
-                    import in ArrayUp
+                if True:#trying to get program to recognize spin up and spin down lines
+                    fh.read('Spin.Up')
+                    store in ArrayUp  #this might only store the lines that define the bands, not the actual lines that contain data
+                    if
                 else:
-                    import in ArrayDown
+                    store in ArrayDown
+                    if
 return ArrayUp
 return ArrayDown
 print ArrayUp
