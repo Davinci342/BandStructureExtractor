156cdddb26eec2e10cbec180fed47f08bc899466
Davinci342 <N00933776@ospreys.unf.edu>
2017-06-06 15:56:44 -0400

    Update BSexport.py

--- BSexport.py
+++ BSexport.py
@@ -7,35 +7,42 @@ Created on Mon Jun  5 12:26:42 2017
 
 fh = open("test.txt","r") 
 import numpy as np
-BandNumber= 1641
+BandNumber= 200,324
 c_comment = '#'
 ArrayUp = np.spinup[BandNumber].reshape[BandNumber,2]
 ArrayDown = np.spindown[BandNumber].reshape[BandNumber,2]
 
 for BandIteration in range(0,BandNumber):
-    fh.readline()
-    def chop_comment(line):#need to look over
-        in_quote = False
-        for i, ch in enumerate(line):
-            if not in_quote and ch == c_comment:
-                return line[:i]
-            return line[:i]
-        return line[:i]
+    #fh.readline()
+    #def chop_comment(line):#need to look over
+        #in_quote = False
+        #for i, ch in enumerate(line):
+            #if not in_quote and ch == c_comment:
+                #return line[:i]
+            #return line[:i]
+        #return line[:i]
     if True:
         fh.readline('c_comment')
         while not '\n\n':
                 if True:#trying to get program to recognize spin up and spin down lines
                     fh.readline('Spin.Up')
-                    np.save in ArrayUp
-                    #if False('c_comment')
-                    #np.save in ArrayUp  #this might only store the lines that define the bands, not the actual lines that contain data
+                    if True:
+                        fh.readline('"____"')
+                        np.save in ArrayUp
                 else:
-                    np.save in ArrayDown
-                    #if False:
-                        #fh.readline('comment')
-                        #np.save in ArrayDown
+                    if True:
+                        fh.readline('"____"')
+                        np.save in ArrayDown
                     return ArrayUp
                     return ArrayDown
-print (ArrayUp)
-print (ArrayDown)
+x = ArrayUp.element
+y = ArrayDown.element
+if True:
+    -2 < x < 2
+    ArrayUp2 = ArrayUp.Where([x]).ToArray()
+if True:
+    -2 < y < 2
+    ArrayDown2 = ArrayDown.Where([x]).ToArray()
+print (ArrayUp2)
+print (ArrayDown2)
 fh.close()
