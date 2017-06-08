312e3b0d9983a4c578231d7d9bb01a3089c1e323
Davinci342 <N00933776@ospreys.unf.edu>
2017-06-08 13:09:56 -0400

    Create BSexport.py

--- BSexport.py
+++ BSexport.py
@@ -8,7 +8,6 @@ Created on Mon Jun  5 12:26:42 2017
 fh = open("test.txt","r") 
 import numpy as np
 BandNumber= 200,324
-c_comment = '#'
 ArrayUp = np.array[BandNumber].reshape[BandNumber,1]
 ArrayDown = np.array[BandNumber].reshape[BandNumber,1]
 #ArrayK = np.array[] /this needs to be defined for the k-points
@@ -23,23 +22,32 @@ for BandIteration in range(0,BandNumber):
                 #return line[:i]
             #return line[:i]
         #return line[:i]
-    if True:
-        fh.readline('c_comment')
-        while not '\n\n':
-                if True:#trying to get program to recognize spin up and spin down lines
-                    fh.readline('Spin.Up')
-                    if True:
-                        fh.readline('"____"')
-                        np.save in ArrayUp
+        fh.readline()
+        line = fh.readline()
+        if '#' in line:
+            while not '\n\n':
+                fh.readline()
+                if 'Spin.Up' in line:#trying to get program to recognize spin up and spin down lines
+                    np.save in ArrayUp
+                    while not '\n\n':
+                        fh.readline()
+                        if '    ' in line:
+                            line.split()
+                            np.save in ArrayUp
                 else:
-                    if True:
-                        fh.readline('"____"')
+                    if 'Spin.Down' in line:
                         np.save in ArrayDown
-                    return ArrayUp
-                    return ArrayDown
+                        while not '\n\n':
+                            fh.readline()
+                            if '    ' in line:
+                                line.split()
+                                np.save in ArrayDown
+        else:
+            pass
+        BandIteration = BandIteration + 1
 x = ArrayUp.element
 y = ArrayDown.element
-if True:
+if True:#need to change;this is incorrect syntax
     -2 < x < 2
     ArrayUp2 = ArrayUp.Where([x]).ToArray()
 if True:
