a4a8affb214cae2814128bc71a6653c325ae45f9
Davinci342 <N00933776@ospreys.unf.edu>
2017-06-14 15:06:26 -0400

    Update BSexport.py

--- BSexport.py
+++ BSexport.py
@@ -6,9 +6,9 @@ Created on Mon Jun  5 12:26:42 2017
 """
 
 fh = open("test.txt","r")
-BS = open("bandstructure.txt","w")
-up = open("spinup.txt","w")
-dn = open("spindown.txt","w")
+BS = open("bandstructure.csv","w")
+up = open("spinup.csv","w")
+dn = open("spindown.csv","w")
 BSlist = []
 list = []
 i = 7564
@@ -18,18 +18,17 @@ j = i+1
 #ArrayK = np.array[] /this needs to be defined for the k-points
 #note: ideally we will want to replace these predefined values with input functions for the user
 for line in fh:
-    fh.readline()
     if line.startswith('\n'):
         pass
-    else:
-        BSlist.append(line)
+    else:             
+        BSlist.append(line) 
 for i in range(len(BSlist[i:i+30])):
-    if i <= 7594:
-        list.append(line.split(BSlist[i]))
-        print(list)
-        if [2] in range(2,-2):
-            BS.write(("BSlist[i+30]"))
-#            str.index(str, beg=0 end=len(string))
+   if i <= 7594:
+       list.append(line.split(BSlist[i]))
+       print(list)
+       print("\n")
+       if [1] in range(2,-2):
+           BS.write((str(BSlist[i+30])))
 fh.close()
 BS.close()
 up.close()
